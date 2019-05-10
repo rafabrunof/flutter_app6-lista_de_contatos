0d37fbf7f143c0dd04eab748f739bb2857b7c3b6
rafabrunof <rafabrunofelix@hotmail.com>
2019-05-10 08:37:23 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'ui/homepage.dart';
+import 'package:contatos/ui/contact_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
